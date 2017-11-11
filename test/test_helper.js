9bdf61e03b8ce4ce9ee586cfe7a3f116767d0e87
David Ramirez <dramirez2096@gmail.com>
2017-11-11 12:14:36 -0800

    before call added test_helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose'); //allows us to use Mongoose
 
 mongoose.Promise = global.Promise; //use Promise library to create promises
 
-mongoose.connect('mongodb://localhost/users_test', {
-    useMongoClient: true,
-}); //mongoose connects to local database "users_test"
-mongoose.connection
-    .once('open', () => console.log('Awesome its working!')) //run this function if mongoose connects
-    .on('error', (error) => {
-        console.warn('Warning', error); //run this function if mongoose does not connect
-    });
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test', {
+        useMongoClient: true,
+    }); //mongoose connects to local database "users_test"
+    mongoose.connection
+        .once('open', () => { done(); }) //run this function if mongoose connects
+        .on('error', (error) => {
+            console.warn('Warning', error); //run this function if mongoose does not connect
+        });
+}); //executed one time before all tests
 
     beforeEach ((done) => {
         mongoose.connection.collections.users.drop(() => {
