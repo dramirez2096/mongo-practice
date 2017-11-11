a1dee0b878f5b5066526d71ac0d63517c9abf4de
David Ramirez <dramirez2096@gmail.com>
2017-11-10 22:49:48 -0800

    save user function added

--- test/create_test.js
+++ test/create_test.js
@@ -3,6 +3,8 @@ const User = require('../src/user')
 
 describe('Creating records', () => { //'Creating records describes the test
     it('saves a user', () => { //'saves a user' describes the test
-        const joe = new User({ name: 'Joe' })
+        const joe = new User({ name: 'Joe' }) //function creates a new user instance named Joe
+
+        joe.save(); //saves joe to database
     });
 });
