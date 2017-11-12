c459008756132d444ff158acabee0b9b6b00485e
David Ramirez <dramirez2096@gmail.com>
2017-11-11 19:42:47 -0800

    find user with .findOne added

--- test/reading_test.js
+++ test/reading_test.js
@@ -13,7 +13,15 @@ describe('Reading users out of the database', () => {
     it('finds all users with the name of "Joe"', (done) =>{
         User.find({ name: 'Joe' }) //finds all objects with this property (name of Joe)
         .then((users) => {
-            assert(users[0]._id.toString() === joe._id.toString());
+            assert(users[0]._id.toString() === joe._id.toString()); //finds user with same string name as user above
+            done();
+        });
+    });
+
+    it('find user with a particular id', (done) => {
+        User.findOne({ _id: joe._id }) //will find only one user named Joe with specific id
+        .then((user) => {
+            assert(user.name === 'Joe');
             done();
         });
     });
