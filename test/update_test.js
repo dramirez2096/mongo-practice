319be76011d06124719b2015dd86eb1649bbe430
David Ramirez <dramirez2096@gmail.com>
2017-11-13 20:41:12 -0800

    increment update operator test created

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({name: 'Joe'});
+        joe = new User({name: 'Joe', postCount: 0});
         joe.save()
             .then(() => done());
     });
@@ -39,5 +39,14 @@ describe('Updating records', () => {
 
     it('findByIdAndUpdate', (done) => {
         assertName(User.findByIdAndUpdate(joe._id, {name:'David'}), done);
+    });
+
+    it('increment test', (done) => {
+        User.update({ name: 'Joe' }, { $inc: {postCount: 1}})
+            .then(() => User.findOne({name:'Joe'}))
+            .then((user) => {
+                assert(user.postCount === 1);
+                done();
+            })
     });
 });
